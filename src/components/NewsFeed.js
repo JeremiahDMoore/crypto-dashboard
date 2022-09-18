72190ba40eacab7f4f552902d5f8b49cd69ec1f5
Jeremiah Moore <jdmenterprises@gmail.com>
2022-09-18 08:19:44 -0700

    news links open in new tab

--- src/components/NewsFeed.js
+++ src/components/NewsFeed.js
@@ -26,13 +26,13 @@ axios.request(options).then((response) => {
 });
     }, [])
     const first7Articles = articles ? articles.slice(10, 17) : null;
-
+    // added target="_blank" to open links in new tab
     return (
       <div className="news-feed">
         
         <h2>Top Stories</h2>
         {first7Articles?.map((article, _index) => (
-        <div key={_index}  >
+        <div key={_index}  > 
             <a href={article.url} target="_blank" rel="noreferrer"><p>{article.title}<br></br>{article.date}</p></a>
         </div>))}
 
