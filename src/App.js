f4f7b89a506ecb4bb6ef7e51f30c3fa35d5c067f
Jeremiah Moore <jdmenterprises@gmail.com>
2023-01-04 08:08:55 -0800

    updated news api call

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import CurrencyConverter from './components/CurrencyConverter';
 import Resources from "./components/Resources";
 // import Ticker from "./components/Ticker"
 // marquee for ticker or reminder, quote, etc.
-const marquee = "Portfolio and LogIn coming soon!"
+const marquee = "Login, Portfolio and more Crypto support coming soon!"
 
 const App = () => {
 
--- src/components/NewsFeed.js
+++ src/components/NewsFeed.js
@@ -3,42 +3,43 @@ import { useState } from "react"
 import axios from "axios"
 
 require('dotenv').config()
-// gets data from newsfeed api
+
 const NewsFeed = () => {
-    const [articles, setArticles] = useState(null);
+  const [articles, setArticles] = useState(null);
 
-    useEffect(() => {
-        
-      const options = {
-        method: 'GET',
-        url: 'https://crypto-news-live3.p.rapidapi.com/news',
-        headers: {
-          'X-RapidAPI-Host': 'crypto-news-live3.p.rapidapi.com',
-          'X-RapidAPI-Key': process.env.REACT_APP_NEWS_FEED_KEY
-        }
-      };
+  useEffect(() => {
+    const options = {
+      method: 'GET',
+      url: 'https://crypto-news16.p.rapidapi.com/news/top/25',
+      headers: {
+        'X-RapidAPI-Key': '14e6db2ea2mshd3417466d04fac6p1ac271jsnc70a82dde267',
+        'X-RapidAPI-Host': 'crypto-news16.p.rapidapi.com'
+      }
+    };
 
-axios.request(options).then((response) => {
-	console.log(response.data)
-    setArticles(response.data)
-}).catch((error) => {
-	console.error(error)
-});
-    }, [])
-    const first7Articles = articles ? articles.slice(10, 35) : null;
-    // added target="_blank" to open links in new tab
-    return (
-      <div className="news-feed">
-        <h2>Today's Top 25</h2>
-        <div className="news-feed-elements">        
+    axios.request(options).then(function (response) {
+      console.log(response.data);
+      setArticles(response.data);
+    }).catch(function (error) {
+      console.error(error);
+    });
+  }, []);
+
+  const first7Articles = articles ? articles.slice(0, 24) : null;
+
+  return (
+    <div className="news-feed">
+      <h2>Today's Top 25 News Stories</h2>
+      
+      <div className="news-feed-elements">
         {first7Articles?.map((article, _index) => (
-        <div key={_index}  > 
+          <div key={_index}  > 
             <a href={article.url} target="_blank" rel="noreferrer"><p>{_index + 1}. {article.title}<br></br>{article.date}</p></a>
-        </div>))}
-        </div>
+          </div>
+        ))}
       </div>
-    )
-  }
-  
-  export default NewsFeed
-  
+    </div>
+  );
+}
+
+export default NewsFeed;
