3c50c08f8796bb1a3ca84c5bad9198985eda6de2
Jeremiah <23569464+JeremiahDMoore@users.noreply.github.com>
2024-11-14 16:35:42 -0700

    Update Resources.js

--- src/components/Resources.js
+++ src/components/Resources.js
@@ -13,7 +13,11 @@ const Resources = () => {
             <a href="https://coinmarketcap.com/" rel = "noreferrer" target = "_blank"><h3>CoinMarketCap - charts, current values, market cap ranking</h3></a>
             <a href="https://coinmarketcal.com/en/" rel = "noreferrer" target = "_blank"><h3>CoinmarketCAL - calendar of crypto events</h3></a>            
             <a href="https://www.coinbase.com/ventures" rel = "noreferrer" target = "_blank"><h3>Coinbase Ventures - what Coinbase is investing in</h3></a>
-            {/* <a href="https://trezor.io/" rel = "noreferrer" target = "_blank"><h3>Trezor</h3></a> */}
+            <a href="https://studio.glassnode.com/home" rel = "noreferrer" target = "_blank"><h3>Glassnode - advanced trading analysis</h3></a>
+            <a href="https://tradingview.com" rel = "noreferrer" target = "_blank"><h3>TradingView - advanced trading analysis</h3></a>
+            <a href="https://www.blockchain.com/explorer" rel = "noreferrer" target = "_blank"><h3>Blockchain Explorer - transactions and realtime blockchain updates</h3></a>
+            <a href="https://etherscan.io/" rel = "noreferrer" target = "_blank"><h3>EtherScan - transactions and realtime blockchain updates</h3></a>
+
         </div>        
       </div>
     )
